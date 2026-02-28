06a63e11c5687ae83c6d27c8902a864925cb5084
ranto-dev <rantoandrianandraina@gmail.com>
2026-02-28 17:58:34 +0000

    feat:verification of input size file and decompressed file extension

--- src/compressor.rs
+++ src/compressor.rs
@@ -1,17 +1,36 @@
-use anyhow::Result;
+use anyhow::{Result, bail};
 use bincode;
 use std::fs;
+use std::path::Path;
 use std::time::Instant;
 
 use crate::{huffman, lz77, stats, ui};
 
+const MIN_SIZE_BYTES: u64 = 100 * 1024 * 1024; // pour 100 MB
+
 pub fn compress_file(input: &str, output: &str) -> Result<()> {
     println!("▶ Compression started");
+
     let start_time = Instant::now();
     let data = fs::read(input)?;
     let original_size = data.len() as u64;
+    let output_path = Path::new(output);
 
-    // LZ77
+    // verification de la taille du fichier input pour la compression
+    if original_size < MIN_SIZE_BYTES {
+        bail!(
+            "File too small. Minimum required size is 100 MB.\nCurrent size: {:.2} MB",
+            original_size as f64 / (1024.0 * 1024.0)
+        );
+    }
+
+    // verification pour le fichier output du compression
+    match output_path.extension() {
+        Some(ext) if ext == "oxp" => {}
+        _ => bail!("Output file must have '.oxp' extension."),
+    }
+
+    // compression avec LZ77
     let spinner = ui::create_spinner("Running LZ77...");
     let lz = lz77::compress(&data);
     spinner.finish_with_message("✔ LZ77 done");
@@ -19,20 +38,20 @@ pub fn compress_file(input: &str, output: &str) -> Result<()> {
     // Serialize LZ77
     let serialized = bincode::serialize(&lz)?;
 
-    // Huffman
+    // compression avec Huffman
     let spinner = ui::create_spinner("Running Huffman...");
     let (encoded, tree) = huffman::compress(&serialized);
     spinner.finish_with_message("✔ Huffman done");
 
-    // Create compressed file
+    // création du fichier compressé
     let spinner = ui::create_spinner("Writing output file...");
     let final_data = bincode::serialize(&(original_size, encoded, tree))?;
     fs::write(output, &final_data)?;
     spinner.finish_with_message("✔ File written");
 
+    // affichage des statistiques de lq compression
     let compressed_size = final_data.len() as u64;
     let duration = start_time.elapsed();
-
     stats::print_stats(original_size, compressed_size, duration);
 
     Ok(())
--- src/decompressor.rs
+++ src/decompressor.rs
@@ -7,25 +7,25 @@ use crate::{huffman, lz77, ui};
 pub fn decompress_file(input: &str, output: &str) -> Result<()> {
     println!("▶ Decompression started");
 
-    // load compressed file
+    // chargement du fichier compressé
     let bytes = fs::read(input)?;
     let spinner = ui::create_spinner("Reading archive...");
     let (original_size, encoded, tree): (u64, Vec<u8>, huffman::HuffmanTree) =
         bincode::deserialize(&bytes)?;
     spinner.finish_with_message("✔ Archive loaded");
 
-    // huffman decompression
+    // décompression avec huffman
     let spinner = ui::create_spinner("Decoding Huffman...");
     let decoded = huffman::decompress(&encoded, &tree);
     spinner.finish_with_message("✔ Huffman decoded");
 
-    // lz77 decompression
+    // décompression avec lz77
     let spinner = ui::create_spinner("Decoding LZ77...");
     let lz_data: Vec<lz77::Token> = bincode::deserialize(&decoded)?;
     let mut result = lz77::decompress(&lz_data);
     spinner.finish_with_message("✔ LZ77 decoded");
 
-    // restructure and save decompressed file
+    // restructuration et sauvegarde du fichier décompressé
     result.truncate(original_size as usize);
     let spinner = ui::create_spinner("Writing output...");
     fs::write(output, result)?;
